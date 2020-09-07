e819142caae556945e937a605e89456b1b1247ad
DJ Poland <djpoland@knights.ucf.edu>
2020-09-06 23:06:01 -0400

    Connect Login frontend to backend

--- js/index.js
+++ js/index.js
@@ -1,12 +1,71 @@
+// Attach event listener to button
+const button = document.getElementById('enterButton');
+button.addEventListener('click', doLogin);
+
+let urlBase = 'http://contactlist27.com/LAMPAPI';
 let extension = 'php';
 
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+
 function doLogin() {
-    // TODO: Set up hashing and login with API endpoint.
+    userId = 0;
+    firstName = "";
+    lastName = "";
+
+    let login = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    // TODO: Implement hashing....
+    // md5();
+
+    // document.getElementById("enterButton");
+
+	let jsonPayload = '{"Login" : "' + login + '", "Password" : "' + password + '"}';
+    let url = urlBase + '/Login.' + extension;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("POST", url, false);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try 
+    {
+        console.log(jsonPayload);
+        xhr.send(jsonPayload);
+
+        let responseText = xhr.responseText;
+        let jsonObject = JSON.parse(responseText);
+        console.log(jsonObject);
+
+        userId = jsonObject.id;
+        if ( userId < 1 )
+        {
+            document.getElementById("enterButton")
+                .innerHTML = "User/Password combination incorrect";
+            return;
+        }
+
+        firstName = jsonObject.firstName;
+        lastName = jsonObject.lastName;
+
+        saveCookie();
+
+        // TODO: Create logged in contactManager.html
+        // window.location.href = "contactManager.html";
+    } 
+    catch (err) 
+    {
+        document.getElementById("enterButton").innerHTML = err.message;
+    }
 }
 
 function saveCookie()
 {
-    // TODO: Save cookie to remind user logged in.
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
 function readCookie() 
