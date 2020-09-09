737b0c34677ff33a519e3024b08748c42f4b0756
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2020-09-09 11:19:44 -0400

    Add additional functions and return statement for testing

--- LAMPAPI/search.php
+++ LAMPAPI/search.php
@@ -27,11 +27,34 @@
 				$searchCount++;
 				$searchResults .= '"' . $row["FirstName"] . '"';
 			}
+			returnWithInfo($searchResults);
 		}
 		else
 		{
 			returnWithError( $conn->error );
 		}
 		$conn->close();
+	}
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
 	}
 ?>
