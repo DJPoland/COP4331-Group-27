de26dec51458679ced3e451accb98d50640e0a9e
DJ Poland <djpoland@knights.ucf.edu>
2020-09-13 18:36:02 -0400

    Cleanup code for readability

--- js/createAccount.js
+++ js/createAccount.js
@@ -1,5 +1,62 @@
 document.getElementById('createAccount').addEventListener('click', doSignup);
 
+function doSignup() {
+    let user = document.getElementById("createUsername").value;
+    let password = document.getElementById("createPassword").value;
+    let confirmPassword = document.getElementById("createConfirmPassword").value;
+    let jsonPayload = '{"Login" : "' + user + '", "Password" : "' + password + '"}';
+    let url = urlBase + '/signup.' + extension;
+
+    if (password !== confirmPassword) {
+        createModalWithMessage(`
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Password combinations differ. Please try again.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>`);
+    } else if ( user.length > 64 || password.length > 64 ) {
+        createModalWithMessage(`
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Password is too long. Please use no more than 16 characters.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>`);
+    } else if ( user.length < 8 || password.length < 8 ) {
+        createModalWithMessage(`
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Password is too short. Please use 8 or more characters.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>`);
+    } else {
+        try {
+            sendSignupPayload(jsonPayload, url);
+            createModalWithMessage(`
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    User and password is successfully created.
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>`);
+        } catch (err) {
+            createModalWithMessage(`
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    Error with signup due to: ${err}.
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>`);
+        }
+    }
+}
+
+function createModalWithMessage(message) {
+    document.getElementById("modalHeader").insertAdjacentHTML('afterend', message);
+}
+
 function sendSignupPayload(jsonPayload, url) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, false);
@@ -9,60 +66,4 @@ function sendSignupPayload(jsonPayload, url) {
     if (jsonObject.error) {
         throw jsonObject.error;
     }
-}
-
-function doSignup() {
-    let user = document.getElementById("createUsername").value;
-    let password = document.getElementById("createPassword").value;
-    let confirmPassword = document.getElementById("createConfirmPassword").value;
-    let modalHeader =  document.getElementById("modalHeader");
-    let jsonPayload = '{"Login" : "' + user + '", "Password" : "' + password + '"}';
-    let url = urlBase + '/signup.' + extension;
-    let warningHtml = undefined;
-
-    if (password !== confirmPassword) {
-        warningHtml = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                Password combinations differ. Please try again.
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>`;
-        modalHeader.insertAdjacentHTML('afterend', warningHtml);
-    } else if ( user.length > 64 || password.length > 64 ) {
-        warningHtml = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                Password is too long. Please use no more than 16 characters.
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>`;
-        modalHeader.insertAdjacentHTML('afterend', warningHtml);
-    } else if ( user.length < 8 || password.length < 8 ) {
-        warningHtml = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                Password is too short. Please use 8 or more characters.
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>`;
-        modalHeader.insertAdjacentHTML('afterend', warningHtml);
-    } else {
-        try {
-            sendSignupPayload(jsonPayload, url);
-            warningHtml = `<div class="alert alert-success alert-dismissible fade show" role="alert">
-                    User and password is successfully created.
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>`;
-            modalHeader.insertAdjacentHTML('afterend', warningHtml);
-        } catch (err) {
-            warningHtml = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    Error with signup due to: ${err}.
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>`;
-            modalHeader.insertAdjacentHTML('afterend', warningHtml);
-        }
-    }
-
 }
--- js/index.js
+++ js/index.js
@@ -1,62 +1,63 @@
 
-let urlBase = 'http://contactlist27.com/LAMPAPI';
-let extension = 'php';
-let userId = 0;
-let firstName = "";
-let lastName = "";
-let password = undefined;
-let button = document.getElementById('enterButton');
+window.urlBase = 'http://contactlist27.com/LAMPAPI';
+window.extension = 'php';
+window.userId = 0;
+window.firstName = "";
+window.lastName = "";
+window.password = undefined;
+window.button = document.getElementById('enterButton');
 
 // Attach event listener to enter button.
-button.addEventListener('click', doLogin);
+window.button.addEventListener('click', doLogin);
 
 function sendLoginPayload(jsonPayload, url) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, false);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     xhr.send(jsonPayload);
+
     let jsonObject = JSON.parse(xhr.responseText);
-    userId = jsonObject.id;
-    if (userId < 1) {
+    window.userId = jsonObject.id;
+    if (window.userId < 1) {
         throw jsonObject.error;
     }
-    firstName = jsonObject.firstName;
-    lastName = jsonObject.lastName;
+    window.firstName = jsonObject.firstName;
+    window.lastName = jsonObject.lastName;
+
     saveCookie();
+
     window.setTimeout(() => {
-        document.getElementById("spinnerHtml").style.display = "none";
-        password.insertAdjacentHTML('afterend',`<br/><br/><span> User found. Redirecting to Contact Manager...</span>`);
-        window.setTimeout(() => window.location.href = "contactManager.html", 5000);
+        window.document.getElementById("spinnerHtml").style.display = "none";
+        window.password.insertAdjacentHTML('afterend',`<br/><br/><span> User found. Redirecting to Contact Manager...</span>`);
+        window.setTimeout(() => window.location.href = "contactManager.html" , 5000);
     }, 1000);
 }
 
 function doLogin() {
 
+    window.userId = 0;
+    window.firstName = "";
+    window.lastName = "";
+
     let spinnerHtml = `<br/><br/><div id="spinnerHtml"><div class="spinner-border" role="status"></div><br/><span>Verifying...</span><br/><br/></div>`;
-
-    userId = 0;
-    firstName = "";
-    lastName = "";
-
-    // TODO: Implement hashing....
-    // md5();
     let loginUser = document.getElementById("username").value;
     let loginPassword = document.getElementById("password").value;
+    // TODO: Implement hashing....
     let jsonPayload = '{"Login" : "' + loginUser + '", "Password" : "' + loginPassword + '"}';
     let url = urlBase + '/Login.' + extension;
 
     // Hide the button and display spinner.
-    button.style.display = "none";
+    window.button.style.display = "none";
 
-    password = document.getElementById("password");
-    password.insertAdjacentHTML('afterend', spinnerHtml);
+    window.password = document.getElementById("password");
+    window.password.insertAdjacentHTML('afterend', spinnerHtml);
     window.setTimeout(() => undefined, 100000);
     try {
         sendLoginPayload(jsonPayload, url);
     } catch (err) {
         window.setTimeout(() => {
-            document.getElementById("spinnerHtml").style.display = "none";
-            password.insertAdjacentHTML('afterend',`<br/><br/><span> User not found. <span/><br/><span> Please try using another Username/Password. </span>`);
+            window.document.getElementById("spinnerHtml").style.display = "none";
+            window.password.insertAdjacentHTML('afterend',`<br/><br/><span> User not found. <span/><br/><span> Please try using another Username/Password. </span>`);
             window.setTimeout(() => location.reload(), 5000);
         }, 1000);
     }
